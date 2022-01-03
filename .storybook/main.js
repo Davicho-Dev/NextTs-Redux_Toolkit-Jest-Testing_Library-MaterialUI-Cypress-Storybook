514d45b8987308623de13dc9811e8a9307111e47
HarmonySkull <developer@harmonyskull.codes>
2022-01-03 04:34:24 -0500

    ADD: TailwindCSS;
    Installing and configuring TailwindCSS and its dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,15 @@ module.exports = {
 	],
 	addons: [
 		'@storybook/addon-links',
-		'@storybook/addon-essentials'
+		'@storybook/addon-essentials',
+		{
+			name: '@storybook/addon-postcss',
+			options: {
+				postcssLoaderOptions: {
+					implementation: require('postcss'),
+				},
+			},
+		},
 	],
 	framework: '@storybook/react',
 }
